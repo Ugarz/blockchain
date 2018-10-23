e338a6ab67563010e6d9dfc36da31cdc63731aca
Ugo <ugo.arzur@morning.com>
2018-10-23 15:34:36 +0200

    Update comments and add a Block mining function

--- core/index.js
+++ core/index.js
@@ -1,13 +1,17 @@
 const crypto = require('crypto');
 const signale = require('signale');
 
+/**
+ * The Block Class
+ */
 class Block {
     /**
      * Block parameters
-     * @param {integer} index Tells where the block is on the chain
-     * @param {string} timestamp Tells when the block was created
-     * @param {Object} data The data to associate with the block
-     * @param {string} previousHash The previous hash (important, ensure integrity)
+     * @class
+     * @param {integer} index - Tells where the block is on the chain
+     * @param {string} timestamp - Tells when the block was created
+     * @param {Object} data - The data to associate with the block
+     * @param {string} previousHash - The previous hash (important, ensure integrity)
      */
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
@@ -19,32 +23,48 @@ class Block {
 
     /**
      * calculateHash the hash based on the previous hash
+     * @memberof Block
      */
     calculateHash() {
-        // const secret = 'abcdefg';
         signale.info('|> Calculating hash..')
         const secret = this.index + this.previousHash + this.timestamp + JSON.stringify(this.data).toString();
         const hash = crypto.createHmac('sha256', secret).digest('hex');
         signale.success('|> hash', hash)
         return hash;
-        // const secret = this.index + this.previousHash + this.timestamp + JSON.stringify(this.data).toString();
-        // console.log('secret', secret)
-        // return SHA256(secret);
+    }
+    /**
+     * Mining Block with it
+     * @returns {Object} minedBlock - The Block mined
+     * @memberof Block
+     */
+    mine(){
+        console.log('Start mining..');
+
+        const mine = (time = 3000) => new Promise((rej,res) => {
+            
+            console.log(`Mining for ${time}ms`);
+            
+            return setTimeout(() => {
+                res({ hash: 'azerty', time })
+            }, time);
+            rej({hash: 'nope'})
+        })
+
+        return mine(200)
     }
 }
 
 /**
- * Our blockhain
+ * The Blockhain Class
  */
 class Blockchain {
-    /**
-     * The Blockchain 
-     */
     constructor() {
         this.chain = [this.createGenesisBlock()];
     }
     /**
      * Create the first Block in the Blockchain
+     * @returns {Object} firstBlock - The first Block
+     * @memberof Blockchain
      */
     createGenesisBlock() {
         return new Block(0, "01/01/2017", "Genesis block", "0");
@@ -53,11 +73,17 @@ class Blockchain {
     /**
      * Get the lastest Block in the Blockchain
      * @returns {array} lastBlock - Return the last block
+     * @memberof Blockchain
      */
     get latestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
+    /**
+     * Add a new Block to the Blockchain
+     * @param {Object} newBlock - Add a new Block 
+     * @memberof Blockchain
+     */
     addBlock(newBlock) {
         // Stock previousBlockHash in currentBlock
         newBlock.previousHash = this.latestBlock.hash;
